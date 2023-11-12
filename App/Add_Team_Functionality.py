78d86db8defe2c3484d64db150dc46caa2b0ad63
Musab1Blaser <musab16000@gmail.com>
2023-11-12 18:41:47 +0500

    linked teams

--- App/Add_Team_Functionality.py
+++ App/Add_Team_Functionality.py
@@ -48,32 +48,50 @@ class AddTeamDialog(QDialog):
             errors.append("Country")
         else:
             selected_row = self.Country_Table.currentRow()
-            selected_country = self.Country_Table.item(selected_row, 0).text()
-            selected_country_code = self.Country_Table.item(selected_row, 1).text()
+            selected_country_code = self.Country_Table.item(selected_row, 0).text()
         
-        # print(selected_row)
-
-        # print(self.Password_Entry.text())
-
         if not len(self.Password_Entry.text()):
             errors.append("Password")
 
-        if len(errors):
+        if len(errors): # Incomplete Info
             self.Error_Label.setText("Please specify " + ", ".join(errors))
             self.Error_Label.show()
         elif not self.Password_Entry.text() == self.Confirm_Password_Entry.text():
             self.Error_Label.setText("Password must match")
             self.Error_Label.show()
-        else:
-            # connection = pyodbc.connect(self.connection_string)
-            # cursor = connection.cursor()
-            uname = f"{selected_country_code.strip()}_{self.Category_ComboBox.currentText()[0]}_{self.Format_ComboBox.currentText()}"
-            print(uname)
-            enc_pwd = hashlib.sha256(uname.encode("utf-8")).hexdigest()
-            print(len(enc_pwd))
-            # cursor.execute("INSERT INTO Power_Users (username, encrypted_password) VALUES (?, ?)", (uname, enc_pwd))
 
-        # self.accept()
+        else:
+            connection = pyodbc.connect(self.connection_string)
+            cursor = connection.cursor()
+
+            team_category = self.Category_ComboBox.currentText()
+            team_format = self.Format_ComboBox.currentText()
+
+            # add new team
+            cursor.execute("INSERT INTO Teams (country_code, category, format) VALUES (?, ?, ?)", (selected_country_code, team_category, team_format))
+            cursor.commit()
+
+            # get team id
+            cursor.execute("SELECT MAX(team_id) FROM TEAMS") 
+            team_id = cursor.fetchone()[0]
+            # print(team_id)
+
+            # Generate username from provided details
+            uname = f"{selected_country_code.strip()}_{team_category[0]}_{team_format}"
+            # print(uname)
+
+            # encrypt password
+            enc_pwd = hashlib.sha256(uname.encode("utf-8")).hexdigest()
+            # print(len(enc_pwd))
+
+            # add power user
+            cursor.execute("INSERT INTO Power_Users (username, encrypted_password, team_id) VALUES (?, ?, ?)", (uname, enc_pwd, team_id))
+            cursor.commit()
+            connection.close()
+
+            QtWidgets.QMessageBox.information(self, "Team Inserted", f"New Team created with username: {uname}.")
+
+            self.accept()
 
 
 class AddCountryDialog(QDialog):
@@ -87,19 +105,19 @@ class AddCountryDialog(QDialog):
         self.Add_Country_Button.clicked.connect(self.add_country)
 
     def add_country(self):
-        country = self.Country_Entry.text()
+        country_name = self.Country_Entry.text()
         country_code = self.Country_Code_Entry.text()
-        print(country, country_code)
+        print(country_name, country_code)
 
         connection = pyodbc.connect(self.connection_string)
         cursor = connection.cursor()
 
-        cursor.execute("INSERT INTO Countries (country_name, country_code) VALUES (?, ?)", (country, country_code))
+        cursor.execute("INSERT INTO Countries (country_code, country_name) VALUES (?, ?)", (country_code, country_name))
         cursor.commit()
 
         # Show a message box with the order ID
         QtWidgets.QMessageBox.information(
-            self, "Country Added", f"Country Name: {country}, Code: {country_code} has been inserted successfully.")
+            self, "Country Added", f"Country Name: {country_name}, Code: {country_code} has been inserted successfully.")
 
         connection.close()
         self.accept()
